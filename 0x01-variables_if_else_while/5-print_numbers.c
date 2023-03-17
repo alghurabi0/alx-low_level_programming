f8ba498704227efc3e755ab386fee3bbfcfdf57f
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-17 10:51:34 -0700

    jkfjfs

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main blocl
+ * Description: print number form 0 to 10
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		printf("%d ", n);
+	}
+
+	putchar('\n');
+	return (0);
+}
