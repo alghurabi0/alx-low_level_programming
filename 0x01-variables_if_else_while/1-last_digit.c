9576ffaf0cd93e83f1b9d34b67511c1fc536a86d
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-17 09:34:42 -0700

    hdiss

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,8 @@
  * main - main block
  * Description: get a random number and print it
  * if it is equal bla bla rquirements in alx task
- * */
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -14,14 +15,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10;
+	last = n % 10;
 
-	if (last_digit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	else if (last_digit < 6)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
-	else
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	if (last > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	else if (last < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
 
 	return (0);
 }
