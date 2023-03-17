47be8fcbeeb9a79845750a6db38aa25da810ae60
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-17 09:54:05 -0700

    kdjfi

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print alphabet in lowercase using putchr
+ * return: 0
+ */
+
+int main ()
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar (c);
+	}
+	return 0;
+}
