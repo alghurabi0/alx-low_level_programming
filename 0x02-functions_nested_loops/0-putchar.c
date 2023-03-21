a1406aece411f994e03a793e99acf0357ec46f1f
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-21 07:50:34 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,23 @@
 #include "main.h"
 /**
  * Main - main block
- * Description: print "_putchar" without stdio.h
+ * Description: print without stdio.h
  * Return: 0
  */
-int main()
+int write();
+int _putchar(char c)
 {
-	test( "_putchar\n" );
-	return 0;
+		return (write(1, &c, 1));
+}
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i = 0;
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
 }
