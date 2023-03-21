b602991cc2aac2de95ba2c97faadfccab1d11819
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-21 06:58:42 -0700

    jfskdijs

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+/**
+ * Main - main block
+ * Description: print "_putchar" without stdio.h
+ * Return: 0
+ */
+int printf(const char *text);
+int main()
+{
+	printf( "_putchar\n" );
+	return 0;
+}
