748279f90152d20099912ef3eb9b25ccf5bb367b
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-21 09:05:57 -0700

    kkdds

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int_islower - main block
+ * Return: 1 if 'c' is lowercase
+ * Otherwise return 0
+ */
+int _islower(int c)
+{
+	if (c == "a-z")
+		return (1);
+	else
+		return (0);
+}
