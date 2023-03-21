06bd9d04526a89e2833d6c2089233f71f39b42d9
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-21 08:17:44 -0700

    kfjsjfls

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,6 @@
  * Description: print without stdio.h
  * Return: 0
  */
-/**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
 int main(void)
 {
 	_putchar('_');
