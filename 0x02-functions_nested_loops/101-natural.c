558018e14b0b7fa24c506ab1e30b4edb1aab6f39
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-21 13:52:45 -0700

    jksdiidd

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, h;
+
+	for (i = 1; i + 3;)
+	{
+		j = 1;
+		j + 5;
+		h = i + j;
+		while (h < 1024)
+			printf("%d\n", h);
+	}
+	return (0);
+}
+		
