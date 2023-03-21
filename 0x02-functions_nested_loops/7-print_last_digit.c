b540402e50f2fa7842088d047d3cdf64d192e704
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-21 12:28:00 -0700

    kjsdlkfs

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - main block
+ * @n : var
+ * Return: lastDigit
+ */
+int print_last_digit(int n)
+{
+	int lastDigit;
+	if (n < 0)
+		lastDigit = -1 * (n % 10);
+	else
+		lastDigit = n % 10;
+	_putchar(lastDigit + '0');
+	return (lastDigit);
+}
