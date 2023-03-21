e46f5fbf45560f77ccbf9be20e3f494675b999a7
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-21 08:29:02 -0700

    kdiiss

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - main block
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
+
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -7,7 +7,10 @@
  *     * Return: On success 1.
  *      * On error, -1 is returned, and errno is set appropriately.
  *       */
+int main() 
+{
 int _putchar(char c)
 {
 		return (write(1, &c, 1));
+}
 }
