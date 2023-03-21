95e310f5ace17d951799eec4f2a5a2db6ef89001
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-21 11:51:51 -0700

    jkdjsis

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - main block
+ * @n : Variable
+ * Return: 0
+ */
+int _abs(int n)
+{
+	int test;
+
+	if (n > 0)
+	{
+		test = 1;
+	}
+	else if (n < 0)
+	{
+		test = 1;
+	}
+	else
+	{
+		test = 0;
+	}
+}
