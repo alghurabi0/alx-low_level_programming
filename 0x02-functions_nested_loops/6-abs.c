8b2e6a8e7b1404147f4cb23328439694ded51d9c
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-21 12:16:04 -0700

    kdjslkjfs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,19 +6,7 @@
  */
 int _abs(int n)
 {
-	int test;
-
-	if (n > 0)
-	{
-		test = 1;
-	}
-	else if (n < 0)
-	{
-		test = 1;
-	}
-	else
-	{
-		test = 0;
-	}
-	return (test);
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
 }
