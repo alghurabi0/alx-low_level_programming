bdc10cc01815a0014aa90aafa40922d781dc43f2
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-21 19:30:54 -0700

    fix of second

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - infinite loop happens because i is not incrementing
+ * main - causes an infinite loop
  * Return: 0
  */
 
@@ -10,15 +10,15 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-/*
-	i = 0;
 
-	while (i < 10)
-	{
-		putchar(i);
-	}
-*/
-	printf("infinite loop happens because i is not incrementing! \\o/\n");
+	i = 0;
+	/*
+	*while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
