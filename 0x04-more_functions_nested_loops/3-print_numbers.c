43ad27f553579ed56faae84798d1f87b9f767e26
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-23 09:01:58 -0700

    print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - main block
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	putchar('\n');
+}
