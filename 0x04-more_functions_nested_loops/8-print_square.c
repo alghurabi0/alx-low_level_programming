2f073961070dc2becbc95520a9c4d3a0a20bfda1
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-23 15:04:19 -0700

    jksjkfsfs

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square - main block
+ * @size: var
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return ;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
