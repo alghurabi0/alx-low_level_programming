639377fbac53b41a9271d66984a3e64bace647ce
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-23 09:31:01 -0700

    hssshs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,15 @@
  */
 void print_most_numbers(void)
 {
-	char i;
+	char i = 0;
 
-	while (i = '0'; i <= '9'; i++)
+	while (i <= 9)
 	{
-		if (i == 2 || i == 4)
-			continue;
-		_putchar(i);
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
