f93fdf7d216aafc36a99474c6404836fe65c9b0f
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-23 15:56:39 -0700

    jkfsjfs

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,20 +3,22 @@
  * main - main block
  * Return: 0
  */
-int main()
+int main(void)
 {
 	int i = 1;
 
 	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		if (i != 100)
+			putchar(' ');
 	}
 	printf("\n");
 	return (0);
