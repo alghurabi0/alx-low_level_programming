1b7326ab3fa7f9845e764db90b6a8c3fda84d0ba
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-23 17:38:48 -0700

    helloddddd

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,19 +8,20 @@ void print_number(int n)
 {
 	int num = n;
 	int power = 1;
+	int num_val;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
-	while (n / power >= 10)
+	while (num / power >= 10)
 	{
 		power *= 10;
 	}
 	while (power > 0)
 	{
-		int num_val = num / power;
+		num_val = num / power;
 		_putchar(num_val + '0');
 		num -= num_val * power;
 		power /= 10;
