2695acc392108684be1222571c02457ddc22d681
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-27 10:20:33 -0700

    put

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - main
+ * Return: 0
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers I am just someting sobsessed lala";
+	_puts(str);
+	return (0);
+}
