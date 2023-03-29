d6d913301ceb22f5f7431adf97bffd2de9f4b0e6
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-29 11:51:32 -0700

    cappp

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,13 +7,19 @@
 char *cap_string(char *str)
 {
 	int i = 0;
+	int j = 0;
 
-	while (str[i] != '\0')
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	if (str[i] >= 'a' && str[i] <= 'z')
+		str[i] -= 32;
+	for (i = 1; i < j; i++)
 	{
 		if (str[i] == 32 || str[i] == 9 || str[i] == 10 || str[i] == 44 || str[i] == 59 || str[i] == 46 || str[i] == 33 || str[i] == 63 || str[i] == 34 || str[i] == 40 || str[i] == 41 || str[i] == 123 || str[i] == 125)
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] = str[i +  1] - 32;
-		i++;
 	}
 	return (str);
 }
