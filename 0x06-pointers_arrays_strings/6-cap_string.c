e9a3d48bf158eb07d48d6922155c791d807cbd24
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-29 12:13:14 -0700

    capotbetyy

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,10 @@ char *cap_string(char *str)
 		str[i] -= 32;
 	for (i = 1; i < j; i++)
 	{
-		if (str[i] == 32 || str[i] == 9 || str[i] == 10 || str[i] == 44 || str[i] == 59 || str[i] == 46 || str[i] == 33 || str[i] == 63 || str[i] == 34 || str[i] == 40 || str[i] == 41 || str[i] == 123 || str[i] == 125)
+		if (str[i] == 32 || str[i] == 9 || str[i] == 10 || str[i] == 44 ||
+		 str[i] == 59 || str[i] == 46 || str[i] == 33 || str[i] == 63 ||
+		 str[i] == 34 || str[i] == 40 || str[i] == 41 || str[i] == 123 ||
+		 str[i] == 125)
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] = str[i +  1] - 32;
 	}
