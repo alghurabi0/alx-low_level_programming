f500e76600cae225a4771ebc940625ee824c6840
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-29 15:43:12 -0700

    leet encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,21 +7,22 @@
 char *leet(char *str)
 {
 	int i, j;
+	char *leet_str = str;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; leet_str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if (str[i] == 'a' || str[i] == 'A')
-				str[i] = '4';
-			else if (str[i] == 'e' || str[i] == 'E')
-				str[i] = '3';
-			else if (str[i] == 'o' || str[i] == 'O')
-				str[i] = '0';
-			else if (str[i] == 't' || str[i] == 'T')
-				str[i] = '7';
-			else if (str[i] == 'l' || str[i] == 'L')
-				str[i] = '1';
+			if (leet_str[i] == 'a' || leet_str[i] == 'A')
+				leet_str[i] = '4';
+			else if (leet_str[i] == 'e' || leet_str[i] == 'E')
+				leet_str[i] = '3';
+			else if (leet_str[i] == 'o' || leet_str[i] == 'O')
+				leet_str[i] = '0';
+			else if (leet_str[i] == 't' || leet_str[i] == 'T')
+				leet_str[i] = '7';
+			else if (leet_str[i] == 'l' || leet_str[i] == 'L')
+				leet_str[i] = '1';
 		}
 	}
 	return (str);
