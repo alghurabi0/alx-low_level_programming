5b33a3a513bf635142665dbd25e4f64afb6eee68
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-28 23:21:29 -0700

    strncat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,7 +18,6 @@ char *_strcat(char *dest, char *src)
 			dest[i] = src[j];
 			j++;
 		}
-
 	}
 	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - main func
+ * @dest: var
+ * @src: var
+ * @n: var
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j;
+
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+	dest[i + j] = '\0';
+	return (dest);
+}
