11fc6d7cc84d84702d68118be09b251c4f54fe89
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-29 15:49:05 -0700

    solving leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,22 +7,19 @@
 char *leet(char *str)
 {
 	int i, j;
-	char *leet_str = str;
+	char leet[] = "aeotl";
+	char LEET[] = "AEOTL";
+	char num[] = "43071";
 
-	for (i = 0; leet_str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if (leet_str[i] == 'a' || leet_str[i] == 'A')
-				leet_str[i] = '4';
-			else if (leet_str[i] == 'e' || leet_str[i] == 'E')
-				leet_str[i] = '3';
-			else if (leet_str[i] == 'o' || leet_str[i] == 'O')
-				leet_str[i] = '0';
-			else if (leet_str[i] == 't' || leet_str[i] == 'T')
-				leet_str[i] = '7';
-			else if (leet_str[i] == 'l' || leet_str[i] == 'L')
-				leet_str[i] = '1';
+			if (str[i] == leet[j] || str[i] == LEET[j])
+			{
+				str[i] = num[j];
+				break;
+			}
 		}
 	}
 	return (str);
