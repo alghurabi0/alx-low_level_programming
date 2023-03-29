719823ca909e7048ba3d5960631b5992950c56c5
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-03-29 15:39:37 -0700

    fix leet code

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,21 +6,23 @@
  */
 char *leet(char *str)
 {
-	int i = 0;
+	int i, j;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		else if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		else if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		else if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		else if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
-		i++;
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == 'a' || str[i] == 'A')
+				str[i] = '4';
+			else if (str[i] == 'e' || str[i] == 'E')
+				str[i] = '3';
+			else if (str[i] == 'o' || str[i] == 'O')
+				str[i] = '0';
+			else if (str[i] == 't' || str[i] == 'T')
+				str[i] = '7';
+			else if (str[i] == 'l' || str[i] == 'L')
+				str[i] = '1';
+		}
 	}
 	return (str);
 }
