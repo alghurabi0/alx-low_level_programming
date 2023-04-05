2e863c64d6bb7973a5850becd3a70e2ee7d5f465
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-04-04 23:48:26 -0700

    fixing task

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,12 +6,9 @@
  */
 int factorial(int n)
 {
-	int subfactorial = factorial(n - 1);
-	int result = n * subfactorial;
-
 	if (n < 0)
 		return (-1);
 	if (n == 0)
 		return (1);
-	return (result);
+	return (n * factorial(n - 1));
 }
