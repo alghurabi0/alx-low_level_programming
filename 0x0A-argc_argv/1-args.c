b9a73513c862705e3cc25d7da281a1950bbb2da2
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-04-08 02:42:52 -0700

    hello world

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
