3054ec3141a77fb26190c8b8015c0294a9010e04
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-04-08 03:48:58 -0700

    changeee

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,14 +8,16 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents = atoi(argv[1]);
 	int num_coins = 0;
+	int cents;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	cents = atoi(argv[1]);
+
 	if (cents < 0)
 	{
 		printf("0\n");
