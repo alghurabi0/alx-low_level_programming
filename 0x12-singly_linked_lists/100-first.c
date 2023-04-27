71209a014bdad432cf466741380c7ed435aa80f1
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-04-27 08:41:44 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * pre_main - prints a message before main function is executed
+ */
+void __attribute__ ((constructor)) pre_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
