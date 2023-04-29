7a82729d3d4d8d82d654dd0b2ff76b9e3930444f
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-04-28 17:10:42 -0700

    f2

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,10 +4,10 @@
  * @h: pointer to the head of the list
  * Return: the number of nodes
  */
-size_t print_list(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - a function that returns the number of elements
+ * in a linked list
+ * @h: a pointer to the head of the list
+ * Return: the number of elements in a linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++
+	}
+	return (count);
+}
