33cf8622748f4c9ce8044e73986f5a0902424088
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-05-08 12:46:31 -0700

    fixing 3rd task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		content_length = 0;
-		while (text_content[content_length] !0 '\0')
+		while (text_content[content_length] != '\0')
 			content_length++;
 		lw = write(fd, text_content, content_length);
 		if (lw == -1)
