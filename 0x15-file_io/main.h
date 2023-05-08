618c0eaee6adab326d923a4100984e3255b6ea31
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-05-08 12:28:22 -0700

    2nd task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,8 +3,13 @@
 
 #include <stddef.h>
 #include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
