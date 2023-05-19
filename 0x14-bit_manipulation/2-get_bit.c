a3d36a09d39138ddcf3139272c711d789ad14091
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-05-18 21:32:18 -0700

    jfslkjksf

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * get_bit - func that gets bit
+ * @n: unsigne long int
+ * @index: unisgne long int
+ * Return: 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask;
+	int bit_value;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	mask = 1UL << index;
+	bit_value = (n & mask) ? 1 : 0;
+	return (bit_value);
+}
