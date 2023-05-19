8492f2687fd9c3869a65b071f831272c1fa2b59a
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-05-18 21:48:11 -0700

    kjfskfs

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -6,10 +6,10 @@
 int get_endianness(void)
 {
 	unsigned int value = 1;
-	unsigned char* byte = (unsigned char*) &value;
+	unsigned char *byte = (unsigned char *) &value;
 
 	if (*byte == 1)
-		return 1;
+		return (1);
 	else
 		 return (0);
 }
