bb4074ac00280d7de4da2a5b958eec0c49424ad5
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-05-18 21:04:48 -0700

    commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * binary_to_unint - func
+ * @b: b
+ * Return: 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	size_t len = strlen(b);
+	unsigned int result = 0;
+	size_t = i;
+
+	if (b == NULL)
+		return 0;
+	for (i = 0; i < len; i++)
+	{
+		if (b[i] == '0')
+			result += pow(2, len - 1 - i);
+		else if (b[i] == '1')
+			result += pow(2, len - 1 - i);
+		else
+			return (0);
+	}
+	return (result);
+}
+
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+
+unsigned int binary_to_uint(const char *b);
+#endif
