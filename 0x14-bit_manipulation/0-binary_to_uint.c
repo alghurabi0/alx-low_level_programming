08931369fe2595c00deddfc3435596de69a5200e
alghurabi0 <zaid.alghurabi01@gmail.com>
2023-05-18 21:22:22 -0700

    jfksjklfjs

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,17 @@ unsigned int binary_to_uint(const char *b)
 {
 	size_t len = strlen(b);
 	unsigned int result = 0;
-	size_t i;
+	int i;
+	unsigned int base = 1;
 
 	if (b == NULL)
-		return 0;
-	for (i = 0; i < len; i++)
+		return (0);
+	for (i = len - 1; i >= 0; i--)
 	{
-		if (b[i] == '0')
-			result += pow(2, len - 1 - i);
-		else if (b[i] == '1')
-			result += pow(2, len - 1 - i);
-		else
+		if (b[i] == '1')
+			result += base;
+		base *= 2;
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
 	return (result);
