e2db29ca649ba7cd708ddfcc98ac6d81bc8c56db
Alghurabi0 <zaid.alghurabi01@gmail.com>
2023-07-10 02:06:33 -0700

    final commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,4 +31,3 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 	return (1);
 }
-                                                                                                                                    ~
