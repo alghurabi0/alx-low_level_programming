e0e60c949d26dc6bb041012d960f7ccd9d4e0261
Alghurabi0 <zaid.alghurabi01@gmail.com>
2023-07-10 07:17:09 -0700

    finishig up

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -18,14 +18,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void print_error_and_exit(const char *message);
-void display_elf_header(const Elf64_Ehdr *header);
-void print_magic(const unsigned char *ident);
-void print_class(unsigned char class);
-void print_data(unsigned char data);
-void print_version(unsigned char version);
-void print_osabi(unsigned char osabi);
-void print_abi_version(unsigned char abi_version);
-void print_type(unsigned int type);
-void print_entry_point(Elf64_Addr entry_point);
 #endif
 
