e85c569c5f518a200a4bcd979163184f7d8bc0ba
Alghurabi0 <zaid.alghurabi01@gmail.com>
2023-08-16 20:43:56 -0700

    doubly linked lists c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * dlistint_len - func that prints all elements in d list
+ * @h: head
+ * Return: number or els
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
