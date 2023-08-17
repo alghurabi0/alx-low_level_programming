8d7bd408bf06c6614896bc30b1cd6600a63dcdf2
Alghurabi0 <zaid.alghurabi01@gmail.com>
2023-08-16 20:40:43 -0700

    c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_dlistint - func that prints all elements in d list
+ * @h: head
+ * Return: number or els
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
