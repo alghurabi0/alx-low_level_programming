f65e8cac1095ab96d679074db22e954d93bdb078
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2023-12-11 13:08:15 +0000

    heey

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,43 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_search - binary search algo
+ * @array: array to search
+ * @size: array size
+ * @value: value to searvh for
+ * Return: index or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i <= right; ++i)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+				printf(", ");
+		}
+		printf("\n");
+
+		mid = (left + right) / 2;
+
+		if (array[mid] == value)
+			return (int)mid;
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,4 +1,6 @@
 #ifndef MY_HEADER
 #define MY_HEADER
+#include <stddef.h>
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 #endif
