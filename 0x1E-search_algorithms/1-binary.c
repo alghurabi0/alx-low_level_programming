ed465bbe0433ec73989cabd4585ecfd14d854e8a
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2023-12-12 23:27:36 +0000

    betty

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -32,7 +32,7 @@ int binary_search(int *array, size_t size, int value)
 		mid = (left + right) / 2;
 
 		if (array[mid] == value)
-			return (int)mid;
+			return ((int)mid);
 		else if (array[mid] < value)
 			left = mid + 1;
 		else
